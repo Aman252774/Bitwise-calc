ed01492dd2e942af278c023d08c8c9ec8a43cba2
Demo_REP <amankumar48219@gmail.com>
2025-07-05 16:12:28 +0530

    bit calcu

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Learn React link', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i); // matches "Learn React" (case-insensitive)
-  expect(linkElement).toBeInTheDocument(); // check if element exists in DOM
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
